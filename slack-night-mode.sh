886215b54d676a65ab2b8a821336cb87875c5666
jburandt <jordanburandt@gmail.com>
2018-10-29 21:07:55 -0400

    add script

---
+++ slack-night-mode.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cat << 'EOF' >> /Applications/Slack.app/Contents/Resources/app.asar.unpacked/src/static/ssb-interop.js
+document.addEventListener('DOMContentLoaded', function() {
+ $.ajax({
+   url: 'https://raw.githubusercontent.com/jburandt/slack-night-mode/master/black.css',
+   success: function(css) {
+     $("<style></style>").appendTo('head').html(css);
+   }
+ });
+});
+EOF
+echo If Slack.app is already running, refresh with CMD-R
+
