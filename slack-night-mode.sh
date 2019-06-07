f9a5ecc1c5ac5a187574c29b2a5cbcbdaebd04c2
Jordan Burandt <jordanburandt@gmail.com>
2019-06-07 10:14:16 -0400

    Update slack-night-mode.sh

--- slack-night-mode.sh
+++ slack-night-mode.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
 
-function applyModification() {
-
-  cat << 'EOF' >> "$1"
-  document.addEventListener('DOMContentLoaded', function() {
-   $.ajax({
-     url: 'https://raw.githubusercontent.com/jburandt/slack-night-mode/master/black.css',
-     success: function(css) {
-       $("<style></style>").appendTo('head').html(css);
-     }
-   });
-  });
-  EOF
-
-}
-
-if [ -f "/Applications/Slack.app/Contents/Resources/app.asar.unpacked/src/static/ssb-interop.js" ]; then
-  applyModification "/Applications/Slack.app/Contents/Resources/app.asar.unpacked/src/static/ssb-interop.js"
-elif [ -f "/usr/lib/slack/resources/app.asar.unpacked/src/static/ssb-interop.js" ]; then
-  applyModification "/usr/lib/slack/resources/app.asar.unpacked/src/static/ssb-interop.js"
-fi 
-echo If Slack.app is already running, refresh with CMD-R
-
+cat << 'EOF' >> /Applications/Slack.app/Contents/Resources/app.asar.unpacked/src/static/ssb-interop.js
+document.addEventListener('DOMContentLoaded', function() {
+ $.ajax({
+   url: 'https://raw.githubusercontent.com/jburandt/slack-night-mode/master/black.css',
+   success: function(css) {
+     $("<style></style>").appendTo('head').html(css);
+   }
+ });
+});
+EOF
